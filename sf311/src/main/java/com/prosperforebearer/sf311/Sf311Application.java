0b2a9a557f0efd02fd57a0ac2265dfb6a04ae320
Paul Sorensen <paulnsorensen@gmail.com>
2017-10-17 18:05:59 -0700

    Initial project from start.spring.io

---
+++ sf311/src/main/java/com/prosperforebearer/sf311/Sf311Application.java
@@ -0,0 +1,12 @@
+package com.prosperforebearer.sf311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf311Application.class, args);
+	}
+}
---
+++ sf311/src/test/java/com/prosperforebearer/sf311/Sf311ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prosperforebearer.sf311;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sf311ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
